f2eaf9c2b2c3c127a30e97e207a04fd5c716271b
benthestudent <45798399+benthestudent@users.noreply.github.com>
2020-01-10 06:34:58 -0800

    updates2

--- screens/PlantScreen.js
+++ screens/PlantScreen.js
@@ -1,16 +1,67 @@
 import React from 'react'
-import {View, StyleSheet, Text, Image} from 'react-native'
-
+import {View, StyleSheet, Text, Button, Image, TouchableWithoutFeedback, Animated} from 'react-native'
+const ACTION_TIMER = 400
+const COLORS = ['rgb(255,255,255)', 'rgb(111,235,62)']
 export default class PlantScreen extends React.Component {
-		static navigationOptions = ( navigation) => {
+	static navigationOptions = ( navigation) => {
 		return {headerTitle: "Plant",
 				headerTitleStyle: { textAlign: 'center', flex:1 },}
 	}
+
+	state = {
+		textComplete: "",
+		value: 0,
+	}
+
+	componentDidMount() {
+	}
+
+	handlePressIn() {
+
+	}
+
+	handlePressOut() {
+
+	}
+
+	animationActionComplete() {
+
+	}
+
 	render() {
 		return (
-			<View>
-				<Image source={require('../assets/tree.png')}/>
+			<View style={styles.container}>
+				<TouchableWithoutFeedback onPressIn={this.handlePressIn} onPressOut={this.handlePressOut}>
+					<View style={styles.container} >
+						<Animated.View />
+						<Text>Press and Hold</Text>
+					</View>
+				</TouchableWithoutFeedback>
+				<View><Text>{this.state.textComplete}</Text></View>
 			</View>
 			)
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+ 		flex: 1, 
+ 		flexDirection: 'column', 
+ 		alignItems: 'center', 
+ 		justifyContent: 'center',
+ 	}, 
+ 	button: { 
+ 		padding: 10, 
+ 		borderWidth: 3, 
+ 		borderColor: '#111' 
+ 	}, 
+ 	text: { 
+ 		backgroundColor: 'transparent', 
+ 		color: '#111' 
+ 	}, 
+ 	bgFill: { 
+ 		position: 'absolute', 
+ 		top: 0, 
+ 		left: 0 
+ 	} 
+ });
--- screens/TriviaScreen.js
+++ screens/TriviaScreen.js
@@ -1,15 +1,58 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, Button} from 'react-native'
 
+class TriviaQuestion extends React.Component {
+		answerPressed(ansID) {
+			if (ansID === 1){
+				console.log("correct")
+			}
+		}
+
+		render(){
+			return (
+				<View>
+					<Text style={this.props.styles.text}>{this.props.question.q}</Text>
+					{this.props.question.answerArray.map(answer => (<Button key={answer.id.toString()} onPress={() => this.answerPressed(answer.id)} style={{backgroundColor: '#fff'}} title={answer.text}/>))}
+				</View>
+				)
+			}
+	}
+const pressed = () => {
+
+}
 export default class TriviaScreen extends React.Component {
 	static navigationOptions = ( navigation) => {
 		return {title: "Trivia"}
 	}
+
+	state = {
+		currentQuestion: 0,
+		questionArray: [{q: "How do you say tree in German?", answerArray: [{id: 1, text: "Der Baum"}, {id: 2, text: "Der Tree"}, {id: 3, text: "Das Bauch"}, {id: 4, text: "Die Holzstück"}],},],
+	}
+
+	
 	render() {
 		return (
-			<View>
-				<Text>Trivia Screen</Text>
+			<View style={styles.container}>
+				<TriviaQuestion styles={styles} question={this.state.questionArray[this.state.currentQuestion]}></TriviaQuestion>
 			</View>
 			)
 	}
 }
+
+const styles = StyleSheet.create({
+		container: {
+			alignItems: "center",
+			justifyContent: "center",
+		},
+		button: {
+ 			padding: 100, 
+		},
+		trivia: {
+			flex: 1,
+		},
+		text: {
+			color: "#F44336",
+			padding: 20
+		}
+	})
